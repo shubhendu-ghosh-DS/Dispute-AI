54a4a00d6d3f614dcf9432e8c0e95f5c62281a27
shubhendu-ghosh-DS <shubhendughosh00@gmail.com>
2026-04-06 10:56:15 +0530

    add sign up sign in flow

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-
-
-
+from api.endpoints.auth import router as auth_router
 
 app = FastAPI()
+
+app.include_router(auth_router)
---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+# user model for authentication and authorization
+from sqlalchemy import Column, Integer, String
+from dependencies.db import Base
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
