c9948ed7f271b809bcb9f1324b7c7add3e2ffed3
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2026-04-09 23:22:18 +0530

    Initialize FastAPI app and include auth router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from api.endpoints.auth import router as auth_router
 
+
+# Initialize fastapi app
 app = FastAPI()
 
+# Include router 
 app.include_router(auth_router)
