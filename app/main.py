3a63add133d58355728913a544bf8d574f1462d7
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2026-04-14 20:10:26 +0530

    Added the complaint router to main file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from api.endpoints.auth import router as auth_router
+from api.endpoints.complaints import router as complaint_router
 
 
 # Initialize fastapi app
@@ -7,3 +8,4 @@ app = FastAPI()
 
 # Include router 
 app.include_router(auth_router)
+app.include_router(complaint_router)
